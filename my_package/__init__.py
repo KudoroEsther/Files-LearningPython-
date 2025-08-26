a1c856ae40b9ca4f9885c1988d9e701505c318b4
Kudoro Esther <kudoroesther.learn@gmail.com>
2025-08-26 15:52:51 +0100

    Added packages

---
+++ my_package/__init__.py
@@ -0,0 +1,5 @@
+print("My package is being imported") #this is printed once the package is imported
+
+#Optional: importing functions directly for easier access
+from .math_utils import add, subtract
+from .string_utils import capitalize_text
