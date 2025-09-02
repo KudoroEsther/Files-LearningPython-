d7aea122699b8d440d400adb56abdc030652a1ed
KudoroEsther <kudoroesther.learn@gmail.com>
2025-09-02 15:53:36 +0100

    Delete ModularizingCode2 directory

--- ModularizingCode2/my_package/__init__.py
+++
@@ -1,5 +0,0 @@
-print("My package is being imported") #this is printed once the package is imported
-
-#Optional: importing functions directly for easier access
-from .math_utils import add, subtract
-from .string_utils import capitalize_text
