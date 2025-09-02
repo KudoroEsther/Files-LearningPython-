e863819d2eb3bf1aaf8a6a56ce1d785795cb7017
Kudoro Esther <kudoroesther.learn@gmail.com>
2025-09-02 15:59:10 +0100

    More housekeeping

--- ModularizingCode2/student_project/utils.py
+++
@@ -1,4 +0,0 @@
-#utils function
-#contains helper functions
-def format_students(student):
-    return f"{student["name"]} is learning {student["track"]} at NCC Digital Innovation Park, Abeokuta."
--- WorkingWithFiles/Task11_ContactSaver.py
+++
@@ -1,6 +0,0 @@
-#CONTACT SAVER
-
-"""
-Main goals: collect participant details: name, age, phone, and track
-Save details into a CSV file
-"""
