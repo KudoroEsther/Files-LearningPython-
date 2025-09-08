49f257f27a6d28e0986a5be13fbfca54c40d5430
Kudoro <kudoroesther.learn@gmail.com>
2025-09-08 15:43:06 +0100

    Added classes

--- Week_5_General/ModularizingYourCode3.py
+++ Week_5_General/ModularizingYourCode3.py
@@ -78,17 +78,123 @@
 # print(student1.name)
 # print(student2.level)
 
-#Class attributes
-class Student:
-    university = "Federal University of Technology Akure"
+# #Class attributes
+# class Student:
+#     university = "Federal University of Technology Akure"
 
-    def __init__(self, name, course):
-        self.name = name
-        self.course = course
+#     def __init__(self, name, course):
+#         self.name = name
+#         self.course = course
     
-student1 = Student("Anthony Joshua", "Engineering")
-student2 = Student("Mary Adeoye", "Medicine")
+# student1 = Student("Anthony Joshua", "Engineering")
+# student2 = Student("Mary Adeoye", "Medicine")
 
-print(Student.university)
-print(student1.university)
-print(student2.university)
+# print(Student.university)
+# print(student1.university)
+# print(student2.university)
+
+
+
+# # #METHODS
+# class Student:
+#     def __init__(self, name, course, level):
+#         self.name = name
+#         self.course = course
+#         self.level = level
+#         self.cgpa = 0.0 
+#         self.fees_paid = False
+    
+#     #Method: action the student can do
+#     def pay_school_fees(self):
+#         self.fees_paid = True
+#         return f"{self.name} has paid school fees for {self.level} level."
+    
+#     def register_courses(self):
+#         if self.fees_paid:
+#             return f"{self.name} has registered courses for {self.course}."
+        
+#     #Method: calculates CGPA
+#     def calculate_cgpa(self, grades):
+#         if grades:
+#             self.cgpa = sum(grades)/len(grades)
+#             return f"{self.name}'s CGPA is now {self.cgpa:.2f}"
+#         return "No grades provided."
+
+
+# # #Using methods
+# # Esther = Student("Esther Kudoro", "Engineering", 500)
+# # print(Esther.pay_school_fees())
+# # print(Esther.register_courses())
+# # print(Esther.calculate_cgpa([4.5, 4.64, 4.89, 4.98, 5.0]))
+
+# #Types of methods
+# #Instance methods: works with specific object data
+# #self refers te the specific student
+#     def pay_school_fees(self):
+#         return f"{self.name} has paid school fees"
+    
+# #Class methods: works with class-level data
+# #this applies the defines method to all instances
+# @classmethod
+# def get_university(cls):
+#     return cls.university
+
+# #Static methods: don't need object or class data
+# #Arguments can be omitted when defining methods within the class
+# @staticmethod
+# def academic_calendar():
+#     return "Academic session runs from September to July."
+
+
+#HOW ATTRIBUTES AND METHODS WORK TOGETHER
+class BankAccount:
+    def __init__(self, owner, bank_name, balance = 0):
+        #Attributs: what the account has
+        self.owner = owner
+        self.bank_name = bank_name
+        self.balance = balance
+        self.account_number = self.generate_account_number()
+
+    #Methods: what the account can do
+    def deposit(self, amount):
+        """Adds money to the account."""
+        if amount > 0:
+            self.balance += amount #Method changes attribute
+            return f"₦ {amount:,} deposited to {self.owner}'s {self.bank_name} account. New balance: ₦{self.balance:,}"
+        return "Invalid deposit amount"
+    
+    def withdraw(self, amount):
+        "Removes money from the account."
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            return f"₦{amount:,} withdrawn from {self.owner}'s account. New balance: ₦{self.balance:,}"
+        return "Insufficient funds or invalid amount."
+    
+    def transfer(self, amount, recipient):
+        """Transfer money to another account"""
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            return f"₦{amount:,} transferred from {self.owner} to {recipient}. Remaining balance: ₦{self.balance}"
+        return "Transer failed: Insufficient funds."
+    
+    def check_balance(self):
+        """Checks current balance"""
+        return f"{self.owner}'s {self.bank_name} account balance: ₦{self.balance:,}"
+    
+    def generate_account_number(self):
+        """Generates a unique account number"""
+        import random 
+        return f"01{random.randint(10000000, 99999999)}"
+    
+#creating and using the account
+esther_account = BankAccount("Esther Kudoro", "Kuda Bank", 5000)
+
+#Attributes (characteristics)
+print(f"Owner: {esther_account.owner}")
+print(f"Account Number: {esther_account.account_number}")
+print(f"Balance: {esther_account.balance}")
+
+#Methods (actions)
+print(esther_account.deposit(250000))
+print(esther_account.withdraw(1000))
+print(esther_account.transfer(15000, "Mary Adeoye"))
